ae69eb458931afbc65e0fa493ab126dd188115b8
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-10-26 08:04:13 -0300

    Add files via upload

---
+++ scripts/Biblioteca.js
@@ -0,0 +1,8 @@
+var Biblioteca ={
+"type": "FeatureCollection",
+"name": "Biblioteca",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Manuel Belgrano" }, "geometry": { "type": "Point", "coordinates": [ -68.843498086461182, -32.924100853036521 ] } }
+]
+}
---
+++ scripts/Hipermercados.js
@@ -0,0 +1,14 @@
+var Hipermercados ={
+"type": "FeatureCollection",
+"name": "Hipermercados",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Libertad" }, "geometry": { "type": "Point", "coordinates": [ -68.856680975196937, -32.929423112915472 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Jumbo" }, "geometry": { "type": "Point", "coordinates": [ -68.83981271943523, -32.917434807298179 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Easy" }, "geometry": { "type": "Point", "coordinates": [ -68.838899935032543, -32.916065779436714 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "Chango Mas" }, "geometry": { "type": "Point", "coordinates": [ -68.8581840268467, -32.951939572690847 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre": "Carrefour Maxi" }, "geometry": { "type": "Point", "coordinates": [ -68.845374619062284, -32.923048621721293 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Nombre": "Maxi Consumo S.A." }, "geometry": { "type": "Point", "coordinates": [ -68.811084351667887, -32.928120670076829 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Nombre": "Yaguar Mayorista" }, "geometry": { "type": "Point", "coordinates": [ -68.81416043510508, -32.929094950203343 ] } }
+]
+}
