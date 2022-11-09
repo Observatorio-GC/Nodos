ed2d3a0c2b91dbf8c3ac107c7a68b79ae1d7cc94
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-11-09 13:41:13 -0300

    Add files via upload

--- scripts/Supermercados.js
+++ scripts/Supermercados.js
@@ -3,12 +3,20 @@ var Supermercados ={
 "name": "Supermercados",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
+{ "type": "Feature", "properties": { "id": 15, "Nombre": "Atomo Estanzuela" }, "geometry": { "type": "Point", "coordinates": [ -68.871349683972355, -32.945405157664787 ] } },
 { "type": "Feature", "properties": { "id": 1, "Nombre": "Carrefour Market Bombal" }, "geometry": { "type": "Point", "coordinates": [ -68.852390888504317, -32.903115158807132 ] } },
 { "type": "Feature", "properties": { "id": 2, "Nombre": "Vea" }, "geometry": { "type": "Point", "coordinates": [ -68.859607970514972, -32.916657069254619 ] } },
 { "type": "Feature", "properties": { "id": 3, "Nombre": "Vea" }, "geometry": { "type": "Point", "coordinates": [ -68.876330180772257, -32.912618851525956 ] } },
 { "type": "Feature", "properties": { "id": 4, "Nombre": "Vea" }, "geometry": { "type": "Point", "coordinates": [ -68.834761979073761, -32.939322328576878 ] } },
 { "type": "Feature", "properties": { "id": 5, "Nombre": "Vea" }, "geometry": { "type": "Point", "coordinates": [ -68.848304656995012, -32.94120424165272 ] } },
 { "type": "Feature", "properties": { "id": 6, "Nombre": "Vea" }, "geometry": { "type": "Point", "coordinates": [ -68.845876650483888, -32.917607211357847 ] } },
-{ "type": "Feature", "properties": { "id": 7, "Nombre": "Vea Cencosud" }, "geometry": { "type": "Point", "coordinates": [ -68.857949745516791, -32.955716863344186 ] } }
+{ "type": "Feature", "properties": { "id": 7, "Nombre": "Vea Cencosud" }, "geometry": { "type": "Point", "coordinates": [ -68.857949745516791, -32.955716863344186 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Nombre": "Atomo Covimet" }, "geometry": { "type": "Point", "coordinates": [ -68.820976750020677, -32.942956342521313 ] } },
+{ "type": "Feature", "properties": { "id": 9, "Nombre": "Atomo Perito Moreno" }, "geometry": { "type": "Point", "coordinates": [ -68.845755444365551, -32.936209008423468 ] } },
+{ "type": "Feature", "properties": { "id": 10, "Nombre": "Atomo Cervantes" }, "geometry": { "type": "Point", "coordinates": [ -68.841816534270095, -32.944684353908173 ] } },
+{ "type": "Feature", "properties": { "id": 11, "Nombre": "Atomo Sportman" }, "geometry": { "type": "Point", "coordinates": [ -68.860874843762673, -32.908981442884816 ] } },
+{ "type": "Feature", "properties": { "id": 12, "Nombre": "Atomo Jardin Serrano" }, "geometry": { "type": "Point", "coordinates": [ -68.878689591330868, -32.919299578601766 ] } },
+{ "type": "Feature", "properties": { "id": 13, "Nombre": "Atomo Castelli" }, "geometry": { "type": "Point", "coordinates": [ -68.848537552670791, -32.927436704487867 ] } },
+{ "type": "Feature", "properties": { "id": 14, "Nombre": "Atomo Villa Marini" }, "geometry": { "type": "Point", "coordinates": [ -68.858876468670218, -32.924128052940418 ] } }
 ]
 }
