0602051be9c2deb0b4b9dbddbfae472d96bf98a9
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-11-25 11:50:52 -0300

    Add files via upload

---
+++ scripts/Arizu.js
@@ -0,0 +1,8 @@
+var Arizu ={
+"type": "FeatureCollection",
+"name": "Arizu",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Ex Bodega Arizu" }, "geometry": { "type": "Point", "coordinates": [ -68.84359102368407, -32.909240600773522 ] } }
+]
+}
